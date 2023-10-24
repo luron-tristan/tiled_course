a765f670a046181d9a0300e1cb268f546d3f8b4d
Tristan Luron <luront@gmail.com>
2023-10-24 13:25:01 +0200

    tiled files

---
+++ data/tsx/tiles.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="128" tileheight="128" tilecount="299" columns="23">
+ <image source="../../_Tiled/graphics/tilesets/tiles.png" width="2944" height="1664"/>
+ <tile id="135" probability="0.25"/>
+ <tile id="136" probability="0.25"/>
+ <tile id="137" probability="0.25"/>
+</tileset>
